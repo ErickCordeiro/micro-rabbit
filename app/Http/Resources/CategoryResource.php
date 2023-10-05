f22881083d1b1c6dbcdca4faa7793ad91d7306d6
Erick Cordeiro <erick.arruda.ext@infracommerce.com.br>
2023-10-05 17:16:01 -0300

    Created Category Model

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->url,
+            'created_at' => $this->created_at,
+            'description' => $this->description
+        ];
+    }
+}
