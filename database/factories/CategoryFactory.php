a261f245226c9407ac65a6a628375a4cdae845fb
Erick Cordeiro <erick.arruda.ext@infracommerce.com.br>
2023-10-23 23:28:43 -0300

    Created Category Test

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Database\Factories\Api;
+namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,6 +11,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CategoryFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     * @var string
+     */
+    protected $model = Category::class;
+
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -17,7 +25,8 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->unique()->title(),
+            'description' => fake()->sentence(200),
         ];
     }
 }
--- tests/Feature/Api/CategoryTest.php
+++ tests/Feature/Api/CategoryTest.php
@@ -2,27 +2,128 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    protected $endpoint = '/categories';
+
     /**
      * Get All Categories
+     *
+     * @return void
      */
-    public function test_get_all_category(): void
+    public function test_get_all_categories()
     {
-        $response = $this->getJson('/categories');
+        Category::factory()->count(6)->create();
+        $response = $this->getJson($this->endpoint);
+        $response->assertJsonCount(6, 'data');
         $response->assertStatus(200);
     }
 
     /**
-     * Get Unique Category
+     * Error get single category
+     *
+     * @return void
      */
-    public function test_get_category(): void
+    public function test_error_single_category()
     {
-        $response = $this->getJson('/categories');
-        $response->assertStatus(200);
+        $category = 'fake-uri';
+        $response = $this->getJson("{$this->endpoint}/{$category}");
+        $response->assertNotFound();
+    }
+
+    /**
+     * Get single category
+     *
+     * @return void
+     */
+    public function test_get_single_category()
+    {
+        $category = Category::factory()->create();
+        $response = $this->getJson("{$this->endpoint}/{$category->url}");
+        $response->assertOk();
+    }
+
+    /**
+     * Validation Store category
+     *
+     * @return void
+     */
+    public function test_validation_store_category()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'title' => '',
+            'description' => ''
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Store category
+     *
+     * @return void
+     */
+    public function test_store_category()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'title' => 'Categoria 01',
+            'description' => 'description of category'
+        ]);
+
+        $response->assertCreated();
+    }
+
+    /**
+     * Update category
+     *
+     * @return void
+     */
+    public function test_update_category()
+    {
+        $category = Category::factory()->create();
+
+        $data = [
+            'title' => 'Category update',
+            'description' => 'Description of category upload'
+        ];
+
+        //Validate category not found (404) - invalid uri
+        $response = $this->putJson("{$this->endpoint}/fake-category", $data);
+        $response->assertNotFound();
+
+        //Validate category with data empty and uri incorrect
+        $response = $this->putJson("{$this->endpoint}/fake-category", []);
+        $response->assertStatus(422);
+
+        //Validate category with data empty and uri correct
+        $response = $this->putJson("{$this->endpoint}/{$category->url}", []);
+        $response->assertStatus(422);
+
+        //Validate category correct
+        $response = $this->putJson("{$this->endpoint}/{$category->url}", $data);
+        $response->assertOk();
+    }
+
+    /**
+     * Delete category
+     *
+     * @return void
+     */
+    public function test_delete_category()
+    {
+        $category = Category::factory()->create();
+
+        //Validate category not found (404) - invalid uri
+        $response = $this->deleteJson("{$this->endpoint}/fake-category");
+        $response->assertNotFound();
+
+        //Validate category correct
+        $response = $this->deleteJson("{$this->endpoint}/{$category->url}");
+        $response->assertNoContent();
     }
 }
